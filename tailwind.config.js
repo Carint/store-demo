33a47a4015b1452f1a5e416b79996a66b8edae7d
Carlos Interiano <carinthnd@gmail.com>
2025-09-25 20:34:43 -0600

    Integrate Tailwind CSS and update layout styles. Added Tailwind integration to the Astro configuration for improved styling capabilities. Updated global styles and layout components to utilize Tailwind's utility classes, enhancing the overall design and responsiveness of the application. Improved search functionality in the catalog page with refined layout and styling adjustments.

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'neutral': {
+          50: 'hsl(var(--neutral-50))',
+          100: 'hsl(var(--neutral-100))',
+          200: 'hsl(var(--neutral-200))',
+          300: 'hsl(var(--neutral-300))',
+          400: 'hsl(var(--neutral-400))',
+          500: 'hsl(var(--neutral-500))',
+          600: 'hsl(var(--neutral-600))',
+          700: 'hsl(var(--neutral-700))',
+          800: 'hsl(var(--neutral-800))',
+          900: 'hsl(var(--neutral-900))',
+          950: 'hsl(var(--neutral-950))',
+        },
+        'accent': {
+          50: 'hsl(var(--accent-50))',
+          100: 'hsl(var(--accent-100))',
+          200: 'hsl(var(--accent-200))',
+          300: 'hsl(var(--accent-300))',
+          400: 'hsl(var(--accent-400))',
+          500: 'hsl(var(--accent-500))',
+          600: 'hsl(var(--accent-600))',
+          700: 'hsl(var(--accent-700))',
+          800: 'hsl(var(--accent-800))',
+          900: 'hsl(var(--accent-900))',
+          950: 'hsl(var(--accent-950))',
+        }
+      }
+    }
+  },
+  plugins: [],
+};
